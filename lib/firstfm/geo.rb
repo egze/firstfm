21a85f9d654469c215372e0855c40ecace0aec6d
Aleksandr Lossenko <aleksandr.lossenko@gmail.com>
2010-03-10 22:18:34 +0100

    make geo.get_events return a will_paginate collection

--- lib/firstfm/geo.rb
+++ lib/firstfm/geo.rb
@@ -10,11 +10,17 @@ module Firstfm
       location = params.delete(:location)
       lat = params.delete(:lat)
       lng = params.delete(:lng)
-      page = params.delete(:page)
+      page = params.delete(:page) || 1
       distance = params.delete(:distance)
       
       response = get("/2.0/", {:query => {:method => 'geo.getevents', :location => location, :page => page, :lat => lat, :lng => lng, :distance => distance, :api_key => Firstfm::CONFIG['api_key']}})
       events = response && response['lfm'] ? Event.init_events_from_hash(response['lfm']) : []
+      
+      collection = WillPaginate::Collection.create(page, 10) do |pager|
+        pager.replace events
+        pager.total_entries = response['lfm']['events']['total'].to_i
+      end
+      
     end
     
   end
